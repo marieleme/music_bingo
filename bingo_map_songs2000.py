844ffd7659cd494bf05d96933d8279108466b642
marieleme <mel062@uit.no>
2019-03-21 16:14:16 +0100

    something

--- bingo_map_songs2000.py
+++ bingo_map_songs2000.py
@@ -1,7 +1,7 @@
 import random
 
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 
 COLOUR_MAP = {
     range(-10, 0): (255, 0, 0),
@@ -41,30 +41,87 @@ def map_song_to_grid(value_grid):
 
     return [[find_song(x, song_list) for x in row] for row in value_grid]
 
+"""
+Change song and artist tuple to pretty strings
+"""
 def beautify(song_tuple, max_width):
     str1 = song_tuple[0]
     str2 = song_tuple[1]
 
     if(len(song_tuple[0]) > max_width):
-        str1 = str1[max_width:] + "- \n" + str1[:]
+        str1 = str1[:max_width] + "- \n" + str1[max_width:]
 
     if(len(song_tuple[1]) > max_width):
-        str2 = song_tuple[1].replace(' ', '\n')
+        str2 = str2[:max_width] + "- \n" + str2[max_width:]
 
-    return str(str1)+"\n"+str(str2)
+    return '"' + str(str1) + '"' +"\n"+str(str2)
 
 def draw_image_from_colour_grid(colour_grid):
-    im = Image.new('RGB', (1500, 1500), (255,255,255))
-    #im.putdata([x for row in colour_grid for x in row])
-    d = ImageDraw.Draw(im)
+    width = 1000
+    height = 800
+    opacity = 0.8
+    transparent = (0, 0, 0, 0)
+    white = (255,255,255)
+    black = (0,0,0)
+    font = ImageFont.truetype('Oswald-Regular.ttf',15)
+
+    wm = Image.new('RGBA',(width,height),transparent)
+    im = Image.new('RGBA',(width,height),transparent)
+
+    draw = ImageDraw.Draw(wm)
 
     for i, row in enumerate(colour_grid):
         for j, song in enumerate(row):
-            some = beautify(song, 15)
-            d.text((i*200,j*100), some, fill=(0,0,0))
+            some = beautify(song, 25)
+            draw.text((i*200, j*150), some, black, font)
 
-    im.save('out.jpg', 'JPEG')
 
+    en = ImageEnhance.Brightness(wm)
+    mask = en.enhance(1-opacity)
+
+    im.paste(wm, (25,25), mask)
+    im.save('bingo.png', 'PNG')
+    
+    #make_white_pixles_transparent('bingo.jpg')
+
+    #paste_grid_to_sheet('sheet.png', 'img2.png')
+
+"""
+def make_white_pixles_transparent(image):
+
+    img = Image.open(image)
+    img = img.convert("RGBA")
+    datas = img.getdata()   
+
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    img.save("img2.png", "PNG")
+"""
+"""
+def paste_grid_to_sheet(sheet, image):
+
+    #Image on which we want to paste 
+    img1 = Image.open(sheet)  
+    
+    if img1.mode!='RGBA':
+        alpha = Image.new('L', img1.size, 255)
+        img1.putalpha(alpha)
+
+    #Relative Path 
+    #Image which we want to paste 
+    img2 = Image.open(image)  
+    #img1.paste(img2, (50, 50)) 
+        
+    #Saved in the same relative location 
+    #img1.save("bingo_sheet.png", "PNG") 
+    #Image.alpha_composite(img1, img2).save("test3.png")
+"""
 
 
 if __name__ == '__main__':
