4f4fa0c7c25f4cfcd0fbde9d68d2f6572419b8e4
Mariel Ellingsen <mel062@post.uit.no>
2020-01-22 13:13:09 +0100

    uses config file, append rows in playlist as bytes

--- read_spotify_list.py
+++ read_spotify_list.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
+
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy 
 import spotipy.util as util
 import json
+import config as cf
+
+client_credentials_manager = SpotifyClientCredentials(client_id = cf.CLIENT_ID, client_secret = cf.CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 
-client_credentials_manager = SpotifyClientCredentials(client_id='b7ddf409ea2447e2b51d3a9013fdc65e', client_secret='bed965d091b341b8846cca8a0f34db5d')
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-# spotify:user:1111932445:playlist:0EamcIJwK2nZWS6iKcNwZ5
-
-uri = 'spotify:playlist:7Im8oz8h3jkrOqMPIcZTpb'
+uri = cf.PLAYLIST_URI
 username = 'spotify'
 playlist_id = uri.split(':')[2]
 
 def show_tracks(results):
     for i, item in enumerate(results['items']):
         track = item['track']
-        with open("something.txt", "a") as file:
+        with open("formated_playlist.txt", "ab") as file:
             string = ("%d:%s:%s \n" % (i, track['name'], track['artists'][0]['name'])).encode('utf-8')
             file.write(string)
 
