25f66c4c902cf226faa39615fcbe9d012a09b064
Elias E. G. Riise <eliasriise@outlook.com>
2022-10-05 20:09:43 +0200

    :art: improve code and add feature for customizing logo, new script to delete files written in python (and now also uses config values)

--- delete_sheets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -n "Removing bingo sheets in BINGO/ folder and merged_bingo_sheets.pdf"
-cd BINGO/ && rm -rf * && cd .. && rm merged_bingo_sheets.pdf
-echo "  ....Done"
-
-echo -n "Removing formatted_playlist.txt"
-rm formatted_playlist.txt
-echo "  ....Done"
-
