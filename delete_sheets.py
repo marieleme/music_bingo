278adec03ded8f615cb9b9e17cea19d70fba3f36
Mariel Ellingsen <mariel@woid.no>
2022-12-14 14:01:42 +0100

    :sheep: fixing typoes, it's working now at least

--- delete_sheets.py
+++ delete_sheets.py
@@ -1,9 +1,9 @@
 from subprocess import run
 import config as cf
 
-print("Removing bingo sheets in " + cf.BINGO + " folder and " + cf.MERGED_BINGO_SHEETS)
+print("Removing bingo sheets in " + cf.BINGO_FOLDER + " folder and " + cf.MERGED_BINGO_SHEETS)
 run(['rm', '-rf', cf.BINGO_FOLDER])
-run(['rm',  + cf.MERGED_BINGO_SHEETS])
+run(['rm', cf.MERGED_BINGO_SHEETS])
 print('....Done')
 
 print("Removing " + cf.PLAYLIST)
